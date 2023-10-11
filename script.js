4ca7507a25cf4fbf384fac13082ea346d158f7d0
ivanjo2004 <142226160+ivanjo2004@users.noreply.github.com>
2023-10-11 17:54:12 -0300

    correccion 2da entrega

---
+++ script.js
@@ -0,0 +1,21 @@
+function sumar(num1,num2){
+    return num1 + num2;
+}
+console.log(sumar(1,2))
+
+function imc (peso,altura){
+    let imc = peso/altura;
+    return imc
+}
+console.log(imc(60,1.6))
+
+function comparar (a,b){
+    return a==b,a===b
+}
+console.log(comparar("5",5))
+
+function par (num){
+    let par;
+    par=num%2;
+    
+}
