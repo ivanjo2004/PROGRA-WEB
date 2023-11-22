718c46a39730c1acf81d5b1808211dde9d203699
ivanjo2004 <142226160+ivanjo2004@users.noreply.github.com>
2023-11-22 10:32:04 -0300

    f

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    const botonesComprar = document.querySelectorAll(".comprar");
-    const totalElemento = document.getElementById("total");
-    let total = 0;
-  
-    botonesComprar.forEach((boton) => {
+  const botonesComprar = document.querySelectorAll(".comprar");
+  const borrarBotones = document.querySelectorAll('.borrar');
+  const totalElemento = document.getElementById("total");
+  let total = 0;
+
+  botonesComprar.forEach((boton) => {
       boton.addEventListener("click", function () {
-        const precio = parseFloat(boton.getAttribute("data-precio"));
-        total += precio;
-        totalElemento.textContent = `$${total.toFixed(2)}`;
+          const precio = parseFloat(boton.getAttribute("data-precio"));
+          total += precio;
+          totalElemento.textContent = `$${total.toFixed(2)}`;
       });
-    });
   });
 
+  borrarBotones.forEach((boton) => {
+      boton.addEventListener("click", function () {
+          const precio = parseFloat(boton.getAttribute("data-precio"));
+          total -= precio;
+          totalElemento.textContent = `$${total.toFixed(2)}`;
+      });
+  });
+});
